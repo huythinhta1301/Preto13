f6fad164d0cc2dba7d90415f300a3786b068db4b
HUYTHINH <ldcc.thinhta@lotte.vn>
2023-06-17 20:43:39 +0700

    Revert "Revert "INIT""
    This reverts commit ee124f5b3cbdf252a7fd9fa000700722555bc76a.

---
+++ Preto13/Controllers/Member/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Preto13.Controllers.Member
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+
+    }
+}
---
+++ Preto13/Data/iRepo/Member/iMemberAccount.cs
@@ -0,0 +1,7 @@
+﻿namespace Preto13.Data.iRepo.Member
+{
+    public interface iMemberAccount
+    {
+        public string SHOW_MEMBER(string memberId);
+    }
+}
